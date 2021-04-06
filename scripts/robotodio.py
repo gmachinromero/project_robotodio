6087454e7f4f97205a00cd27cbc5935414a29874
Guillermo Machín <gmachin@stratio.com>
2021-04-06 19:13:02 +0200

    Update robotodio.py

--- scripts/robotodio.py
+++ scripts/robotodio.py
@@ -2,6 +2,7 @@
 # ------------------------------------------------------------------------------
 # Enviroment
 import os
+import time
 from sinfo import sinfo
 
 # Reading files with different formats
@@ -26,8 +27,174 @@ import tweepy
 from detoxify import Detoxify
 
 
+# API Twitter credentials
+# ------------------------------------------------------------------------------
+# Open .json file containing credentials/tokens as a dictionary
+with open("twitter_api_keys.json") as file:
+    api_credentials = json.load(file)
+    
+# Assign each value of the dictionary to a new variable
+consumer_key = api_credentials['consumer_key']
+consumer_secret = api_credentials['consumer_secret']
+access_token = api_credentials['access_token']
+access_token_secret = api_credentials['access_token_secret']
 
 
-print("Hello world")
 
-st.title('My first app')
+# API set up
+# ------------------------------------------------------------------------------
+# Create a handler instance with key and secret consumer, and pass the tokens
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+    
+# Construct the API instance
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+# Check credentials
+if(api.verify_credentials):
+    print('-'*30)
+    print("Logged In Successfully.")
+    print('-'*30)
+else:
+    print("Error -- Could not log in with your credentials.")
+
+
+
+# Appeareance
+st.set_page_config(layout="wide")
+
+# Title
+st.write("""
+# Robotodio
+Analizando la agresividad en Twitter
+""")
+
+# Sidebar
+st.sidebar.title('Introduce una cuenta de Twitter')
+target = st.sidebar.selectbox('sin @', ('DFD_74', 'Alvisepf'))
+
+
+
+# Tweets list iterator
+# ------------------------------------------------------------------------------
+def tweets_iterator(target, n_items):
+    '''
+    Returns an iterator of tweets.
+
+        Parameters:
+            target (str): The user name of the Twitter account.
+            n_items (int): Number of tweets downloaded.
+
+        Returns:
+            tweets (ItemIterator): an iterator of tweets.
+    '''
+    # Instantiate the iterator
+    tweets = tweepy.Cursor(
+        api.user_timeline,
+        screen_name=target,
+        include_rts=False,
+        exclude_replies=False,
+        tweet_mode='extended').items(n_items)
+    
+    # Returns iterator
+    return tweets
+
+
+
+# Tweet extractor
+# ------------------------------------------------------------------------------
+# Tweets list (iterator)
+tweets = tweets_iterator(target, n_items=20)
+
+# Read through the iterator, and export the info to a Pandas DataFrame
+all_columns = [np.array([
+    tweet.full_text,
+    tweet.user.screen_name,
+    tweet.id,
+    tweet.user.followers_count,
+    tweet.source,
+    tweet.created_at,
+    tweet.lang,
+    len(tweet.full_text),
+    tweet.favorite_count,
+    tweet.retweet_count,
+    re.findall(r"#(\w+)", tweet.full_text)
+]) for tweet in tweets]
+
+# Export the list of tweets to a dataframe
+df = pd.DataFrame(
+    data=all_columns,
+    columns=['tweet', 'account', 'id', 'followers', 'source', 'date', 'language',
+             'length', 'likes', 'RTs', 'hashtags'])
+
+
+
+# Account data extractor
+# ------------------------------------------------------------------------------
+# Tweets list (iterator)
+tweets = tweets_iterator(target, n_items=1)
+
+# Read through the iterator, and export the info to a Pandas DataFrame
+data_account = [np.array([
+    tweet.user.screen_name,
+    tweet.user.name,
+    tweet.user.description,
+    tweet.user.created_at,
+    tweet.user.friends_count,
+    tweet.user.followers_count,
+    tweet.user.statuses_count
+]) for tweet in tweets]
+
+# Export the list of features to a dataframe
+df_account = pd.DataFrame(
+    data=data_account,
+    columns=['account', 'account_name', 'bio_description', 'creation_date',
+             'friends', 'followers', 'tweets']
+)
+
+df_account = pd.melt(df_account)
+
+
+
+# Data cleaning
+# ------------------------------------------------------------------------------
+# Characters to remove
+spec_chars = ['\n', '\t', '\r']
+# Replace defined characters with a whitespace
+for char in spec_chars:
+    df['tweet'] = df['tweet'].str.strip().replace(char, ' ')
+# Split and re-join each tweet
+df['tweet'] = df['tweet'].str.split().str.join(" ")
+
+
+
+# Hate speech level prediction
+# ------------------------------------------------------------------------------
+# Returns a dictionary with toxicity values of each tweet. The key of the
+# dictionary is called toxicity.
+results = Detoxify('multilingual').predict(list(df['tweet']))
+# Add the new info to the previous DataFrame
+df['toxicity'] = results['toxicity']
+# Define a class for each tweet
+df['class'] = df['toxicity'].apply(lambda toxicity: 'toxic' if toxicity >= 0.5 else 'non-toxic')
+# Calculate average toxicity level
+scoring_average = {'variable': ['avg_toxicity'],
+                   'value': [df['toxicity'].mean()]}
+df_average = pd.DataFrame(scoring_average)
+# Principal target account info
+df_account = pd.concat([df_account, df_average], ignore_index=True)
+# Top 5 toxic tweets
+df_top_5 = df[['account', 'date', 'tweet', 'likes', 'RTs', 'toxicity', 'class']]\
+           .sort_values('toxicity', ascending=False).head()
+
+
+
+
+
+
+
+# Principal target account info
+st.dataframe(df_account)
+# Top 5
+st.dataframe(df_top_5)
+
