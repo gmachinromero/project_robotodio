7d3c41d38bcd474db2743061f22a83e53bd1239c
Guillermo Machín <g.machinromero@gmail.com>
2021-12-19 16:17:39 +0100

    Fix bugs

--- scripts/robotodio.py
+++ scripts/robotodio.py
@@ -44,7 +44,7 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
     
 # Construct the API instance
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
 # Check credentials
 if(api.verify_credentials):
